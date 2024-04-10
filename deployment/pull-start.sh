08dba4c546e34ca71272e46b372f8b24e726908b
Xandon Johnson <johnson.1969@wright.edu>
2024-04-10 18:33:52 -0400

    updated pull-start.sh

--- deployment/pull-start.sh
+++ deployment/pull-start.sh
@@ -1,9 +1,15 @@
 #! /bin/bash
 
-# kill the old container process - stopped & removed
+# stop and remove outdated container and report
 docker stop proj5
 docker remove proj5
-# pull fresh image
+echo "old container removed"
+
+# pull fresh image from DockerHub
 docker pull xjohnsonwsu/3120-cicd:latest
-# run new container by name, with restart automagic
+
+# run new container with name and automatic restart
 docker run -d -p 80:80 --name proj5 --restart always xjohnsonwsu/3120-cicd:latest
+
+# report that all commands have been executed
+echo "Everything's shiny, Captain!"
